1a597babfa270157c41a363535980965526efc33
Bazoka13 <1062236184@qq.com>
2020-08-27 01:05:21 +0800

    Add four Fragments in menu

--- OJ4thinhand/app/src/main/java/com/example/oj4thinhand/MenuActivity.kt
+++ OJ4thinhand/app/src/main/java/com/example/oj4thinhand/MenuActivity.kt
@@ -1,37 +1,39 @@
 package com.example.oj4thinhand
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        initView()
+    }
+    private fun initView() {
+        val one = ProblemFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.index_content_fl,one).commit()
     }
     //Called when user click on the Problem button
     fun watchProblem(view: View){
-        val intent = Intent(this, ProblemActivity::class.java).apply {
-        }
-        startActivity(intent)
+        val one = ProblemFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.index_content_fl,one).commit()
     }
     //Called when user click on the Tutorial button
     fun watchTutorial(view: View){
-        val intent = Intent(this, TutorialActivity::class.java).apply {
-        }
-        startActivity(intent)
+        val one = TutorialFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.index_content_fl,one).commit()
     }
     //Called when user click on the Community button
     fun watchCommunity(view: View){
-        val intent = Intent(this, CommunityActivity::class.java).apply {
-        }
-        startActivity(intent)
+        val one = CommunityFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.index_content_fl,one).commit()
     }
     //Called when user click on the Account button
     fun watchAccount(view: View){
-        val intent = Intent(this, AccountActivity::class.java).apply {
-        }
-        startActivity(intent)
+        val one = AccountFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.index_content_fl,one).commit()
     }
 }
