97bfb94ea815256b179502fd00cf336efdca2873
Bazoka13 <1062236184@qq.com>
2020-08-26 09:06:26 +0800

    Just MainAcctivity

--- OJ4thinhand/app/src/main/java/com/example/oj4thinhand/MainActivity.kt
+++ OJ4thinhand/app/src/main/java/com/example/oj4thinhand/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.oj4thinhand
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    //Called when user click on the Problem button
+    fun watchProblem(view:View){
+        val intent = Intent(this, ProblemActivity::class.java).apply {
+        }
+        startActivity(intent)
+    }
+    //Called when user click on the Tutorial button
+    fun watchTutorial(view:View){
+        val intent = Intent(this, TutorialActivity::class.java).apply {
+        }
+        startActivity(intent)
+    }
+    //Called when user click on the Community button
+    fun watchCommunity(view:View){
+        val intent = Intent(this, CommunityActivity::class.java).apply {
+        }
+        startActivity(intent)
+    }
+    //Called when user click on the login button
+    fun sendLoginMessage(view:View){
+        val intent = Intent(this, LoginActivity::class.java).apply {
+        }
+        startActivity(intent)
+    }
+    //Called when user click on the Account button
+    fun watchAccount(view:View){
+        val intent = Intent(this, AccountActivity::class.java).apply {
+        }
+        startActivity(intent)
     }
 }
